353d6d5b8765a48c7fadab57ea095b9c63da5e09
Muhammad Aliyan Sohaib <108171361+aliyanissohaib@users.noreply.github.com>
2024-03-19 04:57:38 +0500

    Update frontpage.php

--- frontpage.php
+++ frontpage.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="frontpage.css" />
-  <title>Graphical Password Authentin System</title>
+  <title>Graphical Password Authenticn System</title>
 </head>
 
 <body>
